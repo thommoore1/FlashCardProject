3f9c3b9b13b75b7c63cb1908d9b3760c6d954619
Samuel Rokaw <143557677+SamuelRokaw@users.noreply.github.com>
2025-09-11 19:11:03 -0700

    Update UI.cs

--- Flash Card Project/Assets/Scripts/UI.cs
+++ Flash Card Project/Assets/Scripts/UI.cs
@@ -1,3 +1,10 @@
+/*
+Names: Tom Moore and Sammy Rokaw
+Emails: thomoore@chapman.edu rokaw@chapman.edu
+ID: Tom: 2444464 Sammy: 2444664
+Course: GAME245-01
+Assignment 1
+  */ 
 using UnityEngine;
 
 public class UI : MonoBehaviour
