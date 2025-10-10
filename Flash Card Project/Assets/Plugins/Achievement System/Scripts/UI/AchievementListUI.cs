116a070b1aa671697eb0cab85db32fdd66ac77f3
Tom Moore <thommoore@chapman.edu>
2025-10-10 16:08:04 -0700

    Everything working but results

--- Flash Card Project/Assets/Plugins/Achievement System/Scripts/UI/AchievementListUI.cs
+++ Flash Card Project/Assets/Plugins/Achievement System/Scripts/UI/AchievementListUI.cs
@@ -9,39 +9,19 @@ public class AchievementListUI : MonoBehaviour
     [SerializeField] private RectTransform scrollRectViewport;
 
     [Space, Header("Visuals")]
-    [SerializeField] private float fadeTransitionDist = 100f;
+    [SerializeField] private float fadeTransitionDist = 100;
 
     private List<AchievementListItemUI> achievementUis = new List<AchievementListItemUI>();
 
     private void Start()
     {
-        ClearAchievementObjects();
-
-        // Listen for new achievements unlocked this round
-        AchievementEvents.OnAchievementGet += OnAchievementGet;
-        AchievementEvents.OnRoundEnded += _ => ClearAchievementObjects();
-    }
-
-    private void OnDestroy()
-    {
-        // Always unsubscribe to prevent memory leaks
-        AchievementEvents.OnAchievementGet -= OnAchievementGet;
-        AchievementEvents.OnRoundEnded -= _ => ClearAchievementObjects();
+        ClearAchievementObjects(); // Clear achievements manually placed in editor
+        PopulateAchievementsList();
     }
 
     private void Update()
     {
         SetAchievementsOpacity();
-    }
-
-    private void OnAchievementGet(AchievementEvents.OnAchievementGetArgs args)
-    {
-        Achievement achievement = args.AchievementObtained;
-
-        // Create UI for this newly unlocked achievement
-        AchievementListItemUI achievementUI = Instantiate(achievementListItemUIPrefab, achievementUiParent);
-        achievementUis.Add(achievementUI);
-        achievementUI.TrackAchievement(achievement);
     }
 
     private void ClearAchievementObjects()
@@ -50,13 +30,20 @@ public class AchievementListUI : MonoBehaviour
         {
             Destroy(child.gameObject);
         }
-        achievementUis.Clear();
+    }
+
+    private void PopulateAchievementsList()
+    {
+        foreach (Achievement achievement in AchievementManager.Instance.Achievements)
+        {
+            AchievementListItemUI achievementUI = Instantiate(achievementListItemUIPrefab, achievementUiParent);
+            achievementUis.Add(achievementUI);
+            achievementUI.TrackAchievement(achievement);
+        }
     }
 
     private void SetAchievementsOpacity()
     {
-        if (achievementUis.Count == 0) return;
-
         Vector3[] worldCorners = new Vector3[4];
         scrollRectViewport.GetWorldCorners(worldCorners);
         float viewportEdgeTop = worldCorners[2].y;
@@ -64,13 +51,11 @@ public class AchievementListUI : MonoBehaviour
 
         foreach (AchievementListItemUI achievementUI in achievementUis)
         {
-            RectTransform rect = achievementUI.GetComponent<RectTransform>();
-            rect.GetWorldCorners(worldCorners);
+            achievementUI.GetComponent<RectTransform>().GetWorldCorners(worldCorners);
             float achievementEdgeTop = worldCorners[2].y;
             float achievementEdgeBottom = worldCorners[0].y;
 
-            float distanceOutOfBounds = Mathf.Max(achievementEdgeTop - viewportEdgeTop, 0.0f)
-                                      + Mathf.Max(viewportEdgeBottom - achievementEdgeBottom, 0.0f);
+            float distanceOutOfBounds = Mathf.Max(achievementEdgeTop - viewportEdgeTop, 0.0f) + Mathf.Max(viewportEdgeBottom - achievementEdgeBottom, 0.0f);
             float opacity = 1.0f - Mathf.Clamp(distanceOutOfBounds / fadeTransitionDist, 0.0f, 1.0f);
             achievementUI.SetOpacity(opacity);
         }
